f2db29e218403b28048b0fb97e59522b6cd8e9f5
n-ando <Noriaki.Ando@gmail.com>
2023-10-19 12:00:00 +0900

    .

---
+++ NewText/replaceicon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+app_base="NewText"
+for theme in light dark; do
+  app="${app_base}_${theme}.app"
+  cp -R ${app_base}.app ${app}
+  cp icons/icon_${theme}.icns ${app}/Contents/Resources/ApplicationStub.icns
+done
+
