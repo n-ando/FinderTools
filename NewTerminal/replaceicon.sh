4483a8b95e8680d421961add03e02fc49a27f331
n-ando <Noriaki.Ando@gmail.com>
2023-10-23 12:00:00 +0900

    .

--- NewTerminal/replaceicon.sh
+++ NewTerminal/replaceicon.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cp icons/icon.icns NewTerminal.app/Contents/Resources/ApplicationStub.icns
-
+app_base="NewTerminal"
+for theme in light dark; do
+  app="${app_base}_${theme}.app"
+  cp -R ${app_base}.app ${app}
+  cp icons/icon_${theme}.icns ${app}/Contents/Resources/ApplicationStub.icns
+done
 
